d99ffc53a9d65a67a9aeb991c52c3579c8ae62d9
Your Name <kzhub@users.noreply.github.com>
2024-04-30 18:46:29 +0900

    add holiday japan 祝日を返す処理を追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,31 @@
-class AttendancesController < ApplicationController
-  def index
+require 'holiday_japan'
 
+class AttendancesController < ApplicationController
+
+  def index
+    # ?year=2024&month=5 param example
+    year = params[:year] ? params[:year].to_i : Date.today.year
+    month = params[:month] ? params[:month].to_i : Date.today.month
+
+    @holidays = getHolidays(year, month)
+
+    start_date = Date.new(year, month, 1)
+    end_date = Date.new(year, month, -1)
+    @days = (start_date..end_date)
   end
+
+  private
+    def getHolidays(year, month)
+      if year.nil? || month.nil? || year < 2000 || year > 2050 || month < 1 || month > 12
+          today = Date.today
+          year = today.year
+          month = today.month
+      end
+
+      start_date = Date.new(year, month, 1)
+      end_date = Date.new(year, month, -1)
+
+      HolidayJapan.between(start_date, end_date)
+    end
+
 end
