a63f8e17584e9313825dff909d4b6d7fe2695b6a
Your Name <kzhub@users.noreply.github.com>
2024-04-30 17:50:37 +0900

    add attendancesコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  #namespace切ってまとめられるかも
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   root 'pages#index'
 
-  resources :users
+  resources :users do
+    resources :attendances
+  end
 end
