d84b22b14cd69c3b796483ca1dac804f24744aa7
Your Name <kzhub@users.noreply.github.com>
2024-05-01 09:56:06 +0900

    add calender

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,32 +1,12 @@
 require 'holiday_japan'
-
 class AttendancesController < ApplicationController
 
   def index
     # ?year=2024&month=5 param example
-    year = params[:year] ? params[:year].to_i : Date.today.year
-    month = params[:month] ? params[:month].to_i : Date.today.month
+    year = params[:year].to_i
+    month = params[:month].to_i
 
-
-    start_date = Date.new(year, month, 1)
-    end_date = Date.new(year, month, -1)
-
-    @holidays = getHolidays(year, month)
-    @days = (start_date..end_date)
+    @holidays = Attendance.getHolidays(year, month)
+    @days = Attendance.getCalender(year, month)
   end
-
-  private
-    def getHolidays(year, month)
-      if year.nil? || month.nil? || year < 2000 || year > 2050 || month < 1 || month > 12
-          today = Date.today
-          year = today.year
-          month = today.month
-      end
-
-      start_date = Date.new(year, month, 1)
-      end_date = Date.new(year, month, -1)
-
-      HolidayJapan.between(start_date, end_date)
-    end
-
 end
