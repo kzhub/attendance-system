aeeb128ddb8270b94d85879769997c0fccf613fc
Your Name <kzhub@users.noreply.github.com>
2024-05-01 13:15:54 +0900

    add ユーザーに紐付いた出退勤時間を追加できるように変更

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,7 +4,6 @@ class AttendancesController < ApplicationController
   def index
     @attendances = Attendance.where(user_id: 7)
 
-    # ?year=2024&month=5 param example
     year = params[:year].to_i
     month = params[:month].to_i
 
@@ -13,10 +12,35 @@ class AttendancesController < ApplicationController
   end
 
   def new
-
+    @isAttendance = User.last_attendance_endtime_is_nil?(params[:user_id])
+    @user = User.find(params[:user_id])
+    @attendance = @user.attendances.new
   end
 
   def create
+    @Attendance = Attendance.new(
+      attendance_date: Date.today,
+      attendance_starttime: Time.now,
+      attendance_endtime: nil,
+      user_id: params[:user_id]#url構造からuseridを取得
+    )
+
+    if @Attendance.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @attendance = Attendance.find(params[:id])
+    @attendance.attendance_endtime = Time.now
+
+    if @attendance.save
+      redirect_to root_path
+    else
+      redirect_to user_path
+    end
 
   end
 
